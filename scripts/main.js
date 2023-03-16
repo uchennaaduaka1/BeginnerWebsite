f35888dda86808dcfd669232facddb195c898a69
Uchenna Aduaka <uchennaaduaka4@gmaill.com>
2023-03-15 20:16:32 -0400

    files added

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+        myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your nane.');
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.innerHTML = `Mozilla is cool, ` + myName;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name")
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
